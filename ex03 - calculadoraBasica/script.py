01d78e7fcc2ddec2a6acae6748d3e9d46ef34bb5
sionphi <tessermatheus817@gmail.com>
2026-03-31 21:42:25 -0300

    Add files via upload

---
+++ ex03 - calculadoraBasica/script.py
@@ -0,0 +1,9 @@
+num1 = int(input("Digite o primeiro numero: "))
+num2 = int(input("Digite o segundo numero: "))
+
+print(f"A soma dos dois numeros é: {num1 + num2}")
+print(f"A subtração dos dois numeros é: {num1 - num2}")
+print(f"A multiplicação dos dois numeros é: {num1 * num2}")
+print(f"A divisão dos dois numeros é: {num1 / num2}")
+print(f"A divisão inteira dos dois numeros é: {num1 // num2}")
+print(f"O resto da divisão entre os dois numeros é: {num1 % num2}")
