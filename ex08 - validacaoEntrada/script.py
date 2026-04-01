6d53ca7824a09f7a3319a8685620a7545b828646
sionphi <tessermatheus817@gmail.com>
2026-03-31 22:28:12 -0300

    commit

--- ex08 - validacaoEntrada/script.py
+++ ex08 - validacaoEntrada/script.py
@@ -2,7 +2,7 @@ idade = int(input("Digite sua idade: "))
 tensDocumento = bool(input("Tem documento: "))
 estaAcompanhado = bool(input("Está acompanhado: "))
 
-if idade >= 18 and tensDocumento == True or estaAcompanhado == True:
+if (idade >= 18 and tensDocumento == True) or (estaAcompanhado == True):
     print("Entrada permitida")
 else:
     print("Entrada Negada!")
