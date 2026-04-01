1b99b45c7e6fa7876e82ac41bd7a618b8da1bf14
sionphi <tessermatheus817@gmail.com>
2026-03-31 22:35:46 -0300

    commit

--- ex06 - tratamentoTexto/script.py
+++ ex06 - tratamentoTexto/script.py
@@ -3,3 +3,5 @@ nomeCompleto = str(input("Digite seu nome completo: "))
 print(f"O seu nome é: {nomeCompleto.lower()}")
 print(f"O seu nome é: {nomeCompleto.upper()}")
 print(f"O seu nome é: {nomeCompleto.strip()}")
+print(f"O seu nome é: {nomeCompleto.len()}")
+
