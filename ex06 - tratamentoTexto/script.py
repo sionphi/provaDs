e18a6eaf413f593a2d0bd8a5701af3a6afb6b76e
sionphi <tessermatheus817@gmail.com>
2026-03-31 22:00:29 -0300

    Add files via upload

---
+++ ex06 - tratamentoTexto/script.py
@@ -0,0 +1,5 @@
+nomeCompleto = str(input("Digite seu nome completo: "))
+
+print(f"O seu nome é: {nomeCompleto.lower()}")
+print(f"O seu nome é: {nomeCompleto.upper()}")
+print(f"O seu nome é: {nomeCompleto.strip()}")
