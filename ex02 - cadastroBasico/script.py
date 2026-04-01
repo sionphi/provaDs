332dbb0528aa1480239ead8c91e312959cbd7ab4
sionphi <tessermatheus817@gmail.com>
2026-03-31 21:37:39 -0300

    Add files via upload

---
+++ ex02 - cadastroBasico/script.py
@@ -0,0 +1,7 @@
+nome = "Matheus Tesser"
+idade = 16
+aluno_ativo = True
+
+print(f"Nome: {nome}", type(nome))
+print(f"Idade: {idade}", type(idade))
+print(f"Aluno ativo? {aluno_ativo}", type(aluno_ativo))
