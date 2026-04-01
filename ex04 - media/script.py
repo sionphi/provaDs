4477c11b00e30955d08f5b013ba759bf0f31cd2f
sionphi <tessermatheus817@gmail.com>
2026-03-31 21:47:35 -0300

    Add files via upload

---
+++ ex04 - media/script.py
@@ -0,0 +1,6 @@
+notaUm = float(input("Digite a primeira nota: "))
+notaDois = float(input("Digite a segunda nota: "))
+notaTres = float(input("Digite a terceira nota: "))
+media = (notaUm + notaDois + notaTres) / 3
+
+print(f"A media da sua nota foi: {media:.2f}")
